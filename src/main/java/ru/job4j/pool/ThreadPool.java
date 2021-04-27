7f39112dce84d5c385efb623f0d4a114293bb9e7
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-04-27 21:45:50 +0300

    1. Реализовать ThreadPool[1099#512518]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -6,22 +6,49 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ThreadPool {
-    private final int size = Runtime.getRuntime().availableProcessors();
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
 
-    public ThreadPool() throws InterruptedException {
+    public ThreadPool() {
+        int size = Runtime.getRuntime().availableProcessors();
         for (int i = 0; i < size; i++) {
-            threads.add(new Thread(tasks.poll()));
+            threads.add(createConsumer());
             threads.get(i).start();
         }
     }
 
-    public void work(Runnable job) throws InterruptedException {
-        tasks.offer(job);
+    public void work(Runnable job) {
+        new Thread(job).start();
     }
 
     public void shutdown() {
         threads.forEach(Thread::interrupt);
     }
+
+    public static void main(String[] args) {
+        ThreadPool pool = new ThreadPool();
+        for (int i = 1; i < 21; i++) {
+            int finalI = i;
+            pool.work(() -> System.out.println("work №" + finalI));
+        }
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        pool.shutdown();
+    }
+
+    private Thread createConsumer() {
+        return new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    tasks.poll().run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+    }
 }
+
